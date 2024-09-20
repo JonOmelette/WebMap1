7c3e907de1ffff8f225f472acdcc84cf9528495d
joneged <joneged@gmail.com>
2024-09-20 10:54:37 -0700

    Making my first commit ever!

---
+++ R/spatial.R
@@ -0,0 +1,8 @@
+# We can make code that creates a new R file for spatial analysis.
+# This makes a brand new R code file!
+usethis::use_r("spatial") # creates R/spatial.R in your project. You can change the name to other things.
+
+# This will contain the spatial data analysis code for this workshop.
+print("Hello World")
+
+# On the right
